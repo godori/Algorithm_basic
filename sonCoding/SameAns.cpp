df78e1ca35c079bae816fa6d6848bfb7637cfb02
godori <gej.okpo@gmail.com>
2016-05-01 04:11:53 +0900

    SonCoding solved

---
+++ sonCoding/SameAns.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+// 노드 클래스 정의
+class Node{
+public:
+  int val = -1;
+  Node * left;
+  Node * right;
+
+  // 양 노드 삽입
+  void insert(Node * left, Node * right){
+    this->left = left;
+    this->right = right;
+  }
+};
+
+// 공통 조상을 찾는 함수
+Node findCommon(Node *root, Node *first, Node *second){
+  int value1 = first->val;
+  int value2 = second->val;
+
+  while(root->val != -1){
+    int val = root->val;
+    if((val > value1) && (val > value2))
+      root = root->left;
+    else if( (val < value1) && (val < value2))
+      root = root->right;
+    else
+      return *root;
+  }
+   return *root;
+}
+
+int main(){
+
+  // 임의로 트리 구성
+  Node root,l1,r1,l2,r2,l3,r3;
+  root.val = 20;
+  l1.val = 8;
+  r1.val = 22;
+  l2.val = 4;
+  r2.val = 12;
+  l3.val = 10;
+  r3.val = 14;
+  root.insert(&l1,&r1);
+  l1.insert(&l2,&r2);
+  r2.insert(&l3,&r3);
+
+  // 두 노드의 가장 가까운 공통 조상을 찾는다.
+  cout<<"The Lowest Common Ancestor is : ";
+  cout<<findCommon(&root, &l2, &r3).val<<endl;
+
+  return 0;
+}
--- sonCoding/StepsUp.cpp
+++ sonCoding/StepsUp.cpp
@@ -1,29 +1,61 @@
 // n개의 계단을 아이가 오른다. 한번에 1계단 오르기도 하고, 2계단, 3계단 오르기도 한다.
 // 계단을 오르는 모든 경우의 수를 구하는 메서드를 구현하세요.
 // ex) n=3으로 주어지게 되면
-// 1계단+ 1계단+ 1계단
-// 1계단+ 2계단
-// 2계단+ 1계단
+// 1계단 + 1계단+ 1계단
+// 1계단 + 2계단
+// 2계단 + 1계단
 // 3계단
 // 총 4가지 경우가 있는겁니다.
 // 4를 return해주시면 됩니다.
 //  메서드 형식: int countWays(int n);
 
-#include <iostrea>
+#include <iostream>
 using namespace std;
 
-int countWays(int n){
+// Dynamic Programming
+long long s[10001];
 
+// Recursive
+int step(int n){
 
+  int cnt = 0;
 
-  return 0;
+  if(n == 1)
+    return 1;
+  else if (n == 2)
+    return 2;
+  else if (n == 3)
+    return 4;
+  else
+    cnt = step(n-1) + step(n-2) + step(n-3);
+
+  return cnt;
 }
 
 int main(){
-  int num;
-  cin >> num;
+  int num,i;
 
-  cout<<countWays(n);
+  s[0] = 0;
+  s[1] = 1;
+  s[2] = 2;
+  s[3] = 4;
+
+  while (true){
+    cout<<"type number:";
+    cin >> num;
+
+    // calculate steps 1
+    for(i=4; i <= num; i++)
+      s[i] = s[i-1] + s[i-2] + s[i-3];
+
+    cout<<s[num]<<endl;
+
+    // calculate steps 2
+    // if(num == 0)
+    //   cout<<0<<endl;
+    // else
+    //   cout<<step(num)<<endl;
+  }
 
   return 0;
 }
